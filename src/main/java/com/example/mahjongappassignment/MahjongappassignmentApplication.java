36aab4f67bde4dfc968689394e0df687dc1e9d01
zhoube <zhouyikelvin@gmail.com>
2026-03-05 22:04:40 +0800

    Initial Spring Boot project

---
+++ src/main/java/com/example/mahjongappassignment/MahjongappassignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.mahjongappassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MahjongappassignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MahjongappassignmentApplication.class, args);
+    }
+
+}
